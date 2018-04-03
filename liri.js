50b17cce4b36cfea81dffa9843fa1baec112b5ff
errol gingis <errol.gingis@gmail.com>
2018-04-02 21:25:21 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,8 @@
+// Read and set enviornment variable
+
+require('dotenv').config();
+
+console.log(process.env.SPOTIFY_CLIENTID)
+console.log(process.env.SPOTIFY_CLIENTSECRET)
+console.log(process.env.TWITTERKEY)
+console.log(process.env.TWITTERID)
