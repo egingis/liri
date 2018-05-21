d9226560d5e002fcc50b90edbff17071c5873598
egingis <33986417+egingis@users.noreply.github.com>
2018-05-21 15:32:43 -0400

    Add files via upload

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
-// Read and set enviornment variable
+require("dotenv").config();
 
-require('dotenv').config();
+//Create all the variables that pull in the node commands
 
-console.log(process.env.SPOTIFY_CLIENTID)
-console.log(process.env.SPOTIFY_CLIENTSECRET)
-console.log(process.env.TWITTERKEY)
-console.log(process.env.TWITTERID)
+var keys = require('./keys.js');
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
